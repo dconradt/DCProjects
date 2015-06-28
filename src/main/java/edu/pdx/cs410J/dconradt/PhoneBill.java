f317b10ea4dc9271f61aec9abd0f0fad869a181a
dconradt <r8buffalo>
2015-06-28 14:56:54 -0700

    Initial commit

---
+++ src/main/java/edu/pdx/cs410J/dconradt/PhoneBill.java
@@ -0,0 +1,27 @@
+package edu.pdx.cs410J.dconradt;
+
+import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.AbstractPhoneCall;
+
+import java.util.Collection;
+
+/**
+ * Created by Dan Conradt on 6/28/2015.
+ */
+public class PhoneBill extends AbstractPhoneBill {
+    private static String customerName;
+    @Override
+    public void addPhoneCall(AbstractPhoneCall abstractPhoneCall) {
+        
+    }
+
+    @Override
+    public String getCustomer() {
+        return null;
+    }
+
+    @Override
+    public Collection getPhoneCalls() {
+        return null;
+    }
+}
---
+++ src/main/java/edu/pdx/cs410J/dconradt/PhoneCall.java
@@ -0,0 +1,40 @@
+package edu.pdx.cs410J.dconradt;
+
+import edu.pdx.cs410J.AbstractPhoneCall;
+import java.util.Date;
+
+/**
+ * Created by Dan on 6/28/2015.
+ */
+public class PhoneCall extends AbstractPhoneCall {
+
+    @Override
+    public String getCallee() {
+        return null;
+    }
+
+    @Override
+    public String getCaller() {
+        return null;
+    }
+
+    @Override
+    public Date getEndTime() {
+        return super.getEndTime();
+    }
+
+    @Override
+    public Date getStartTime() {
+        return super.getStartTime();
+    }
+
+    @Override
+    public String getEndTimeString() {
+        return null;
+    }
+
+    @Override
+    public String getStartTimeString() {
+        return null;
+    }
+}
