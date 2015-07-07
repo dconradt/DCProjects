97147500619093c6456f97c5bc5925f5ba931aad
dconradt <r8buffalo>
2015-07-07 10:11:56 -0700

    Project 1 Final

--- src/main/java/edu/pdx/cs410J/dconradt/Project1.java
+++ src/main/java/edu/pdx/cs410J/dconradt/Project1.java
@@ -43,75 +43,67 @@ public class Project1 {
         int optionIndex = 0;/** index for the option array */
         boolean success = false; /** Used to verify the success of reading command line arguments. */
         boolean print = false; /** Print flag set when option to print is requested. */
-        boolean readMe = false; /** Sets the README flag to print a README for this project then exits.*/
+
+        /** Check to make sure sufficient arguments are in the command line */
+        if (args.length < 7){
+            System.out.println("\nMissing arguments");
+            System.exit(1);
+        }
+
 
         /** Iterate through the arguments, verify phone number and date/time formats where appropriate and populate the phone call array. */
         for(int i = 0; i < 2 ; ++i) {
-            if (args[i].equalsIgnoreCase("-print")) {
-                options[i] = args[i];
+
+            if (args[i].equalsIgnoreCase("-print"))
+                //options[i] = args[i];
                 print = true;
-            } else if (args[i].equalsIgnoreCase("-README") &&!print) {
+            else if (args[i].equalsIgnoreCase("-README")) {
                 System.out.println("\n***README***\n\nDan Conradt - Project1\n\nI have implemented three classes - Project1, PhoneBill and PhoneCall. PhonebBill and PhoneCall\nextend the AbstractPhoneBill and AbstractPhoneCall classes respectively" +
                         "This program takes\nan input of upto 2 options and requires 5 arguments describing a phone call.  It validates the\noptions and arguments for validity and formatting.  The date and time must be " +
                         "actual dates in the\nform specified as mm/dd/yyyy hh:mm.  The Phone numbers must be of the form nnn-nnn-nnnn.\nErrors in formating or validity of the phone numbers or date and time will output a message\nand the program will exit.  " +
-                        "If all arguments are valid the program will create an new phone call\nrecord using an array list of phone calls in the phone bill class.  If the option -print is\nprovided then the program " +
-                        "will output the phone call from the given parameters.");
+                        "If all arguments are valid the program will create an new phone call\nrecord using an array list of phone calls in the phone bill class.  If the option -README is\nprovided then the program " +
+                        "will output the README text description of the program and then exit.");
                 System.exit(1);
             }
-            else if(args[i].equalsIgnoreCase("-README") ){
-                options[i] = args[i];
-                readMe = true;
+        }
+                if (print)
+                    ++argIndex;
+        try {
+                newBill.setCustomer(args[argIndex]);
+                success = verifyPhoneNumber(args[argIndex + 1]);// verify phone format
+                if (success)
+                    newCall.setCallerNumber(args[argIndex + 1]);
+                else
+                    exitProgram("\nInvalid Command Line Arguments.\nA phone number is invalid or not of the form nnn-nnn-nnnn.");
+                success = verifyPhoneNumber(args[argIndex + 2]); // verify phone format
+                if (success)
+                    newCall.setCalleeNumber(args[argIndex + 2]);
+                else
+                    exitProgram("\nInvalid Command Line Arguments.\nA phone number is invalid or not of the form nnn-nnn-nnnn.");
+                timeStamp = args[argIndex + 3] + " " + args[argIndex + 4];
+                success = verifyDateFormat(timeStamp); // verify data and time format
+                if (success)
+                    newCall.setStartTime(timeStamp);
+                else
+                    exitProgram("\nInvalid Command Line Arguments.\nA date and time must be of the format dd/mm/yyy/ hh:mm");
+                timeStamp = args[argIndex + 5] + " " + args[argIndex + 6];
+                success = verifyDateFormat(timeStamp);// verify data and time format
+                if (success)
+                    newCall.setEndTime(timeStamp);
+                else
+                    exitProgram("\nInvalid Command Line Arguments.\nA date and time must be of the format dd/mm/yyy/ hh:mm");
+                newBill.addPhoneCall(newCall);
+                if (print)
+                    System.out.println("\n" + newCall.toString());
+            } catch (RuntimeException ex) {
+                System.out.print("\nInvalid Command Line Arguments.\n");
+                System.exit(1);
             }
         }
-        if(print)
-            ++argIndex;
-        if(readMe)
-            ++argIndex;
-        newBill.setCustomer(args[argIndex]);
-
-        success = verifyPhoneNumber(args[argIndex + 1]);
-        if(success)
-            newCall.setCallerNumber(args[argIndex + 1]);
-        else
-            exitProgram("The phone number is invalid or the form nnn-nnn-nnnn.");
-
-        success = verifyPhoneNumber(args[argIndex + 2]);
-        if(success)
-            newCall.setCalleeNumber(args[argIndex + 2]);
-        else
-            exitProgram("The phone number is invalid or the form nnn-nnn-nnnn.");
-
-        timeStamp = args[argIndex + 3] + " " + args[argIndex + 4];
-        success = verifyDateFormat(timeStamp);
-        if (success)
-            newCall.setStartTime(timeStamp);
-        else
-            exitProgram("The date and time must be of the format dd/mm/yyy/ hh:mm");
-
-        timeStamp = args[argIndex + 5] + " " + args[argIndex + 6];
-        success = verifyDateFormat(timeStamp);
-        if (success)
-            newCall.setEndTime(timeStamp);
-        else
-            exitProgram("The date and time must be of the format dd/mm/yyy/ hh:mm");
-
-        newBill.addPhoneCall(newCall);
-        if (print)
-            System.out.println("\n" + newCall.toString());
-        if(readMe) {
-            System.out.println("\n***README***\n\nDan Conradt - Project1\n\nI have implemented three classes - Project1, PhoneBill and PhoneCall. PhonebBill and PhoneCall\nextend the AbstractPhoneBill and AbstractPhoneCall classes respectively" +
-                    "This program takes\nan input of upto 2 options and requires 5 arguments describing a phone call.  It validates the\noptions and arguments for validity and formatting.  The date and time must be " +
-                    "actual dates in the\nform specified as mm/dd/yyyy hh:mm.  The Phone numbers must be of the form nnn-nnn-nnnn.\nErrors in formating or validity of the phone numbers or date and time will output a message\nand the program will exit.  " +
-                    "If all arguments are valid the program will create an new phone call\nrecord using an array list of phone calls in the phone bill class.  If the option -print is\nprovided then the program " +
-                    "will output the phone call from the given parameters." );
-            System.exit(1);
-        }
-    }
 
     /**
      * Verify the date formatting is correct. Java regex pattern and matcher are used to describe the required input. Returns the value
      * of the successful or failure of a match.
-     *
      * @param date  Date to be verified
      * @return Return true if date is valid otherwise false
      */
