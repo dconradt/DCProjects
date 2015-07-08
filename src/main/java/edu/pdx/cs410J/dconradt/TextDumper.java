fca7031a65c239c9de11b7da52716d7bb0e7a516
dconradt <r8buffalo>
2015-07-08 08:27:19 -0700

    Project 2 added classes

---
+++ src/main/java/edu/pdx/cs410J/dconradt/TextDumper.java
@@ -0,0 +1,16 @@
+package edu.pdx.cs410J.dconradt;
+
+import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.PhoneBillDumper;
+
+import java.io.IOException;
+
+/**
+ * Created by Dan on 7/8/2015.
+ */
+public class TextDumper implements PhoneBillDumper {
+    @Override
+    public void dump(AbstractPhoneBill abstractPhoneBill) throws IOException {
+
+    }
+}
---
+++ src/main/java/edu/pdx/cs410J/dconradt/TextParser.java
@@ -0,0 +1,15 @@
+package edu.pdx.cs410J.dconradt;
+
+import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.ParserException;
+import edu.pdx.cs410J.PhoneBillParser;
+
+/**
+ * Created by Dan on 7/8/2015.
+ */
+public class TextParser implements PhoneBillParser {
+    @Override
+    public AbstractPhoneBill parse() throws ParserException {
+        return null;
+    }
+}
